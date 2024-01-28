7d289aad3c4b404b7cd99b8d8edc7f5863f57263
Brian Caldera <brian_alex07@outlook.com>
2024-01-28 19:48:36 -0400

    Added Presupuesto model

---
+++ database/migrations/2024_01_28_233922_create_presupuestos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('presupuestos', function (Blueprint $table) {
+            $table->id();
+            $table->text('tratamiento');
+            $table->decimal('costo_unitario', 19, 4);
+            $table->timestamps();
+
+            $table->foreignId('historia_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('presupuestos');
+    }
+};
