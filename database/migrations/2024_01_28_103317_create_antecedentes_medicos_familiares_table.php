e201b1d734310c6d51611ddca7e6d1477322f03d
Brian Caldera <brian_alex07@outlook.com>
2024-01-28 06:41:32 -0400

    Added AntecedentesMedicosFamiliares

---
+++ database/migrations/2024_01_28_103317_create_antecedentes_medicos_familiares_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('antecedentes_medicos_familiares', function (Blueprint $table) {
+            $table->foreignId('historia_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->text('madre');
+            $table->text('padre');
+            $table->text('hermanos');
+            $table->text('abuelos_maternos');
+            $table->text('abuelos_paternos');
+
+            $table->primary('historia_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('antecedentes_medicos_familiares');
+    }
+};
