6a9f6fbb165cd13a101d95f3f632fd2dd177b105
Brian Caldera <brian_alex07@outlook.com>
2024-01-29 03:03:25 -0400

    Added FichaClinicaCirugiaPacienteRegular model

---
+++ database/migrations/2024_01_29_065221_create_ficha_clinica_cirugia_paciente_regulars_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ficha_clinica_cirugia_paciente_regulars', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('cedula');
+            $table->integer('edad');
+            $table->string('telefono');
+            $table->string('ocupacion');
+            $table->text('direccion');
+            $table->text('nombre_bachiller');
+            $table->string('cedula_bachiller');
+            $table->integer('semestre');
+            $table->string('telefono_bachiller');
+            $table->text('motivo_consulta');
+            $table->text('enfermedad_actual');
+            $table->text('variacion_estado_sistemico');
+            $table->text('evaluacion_radiografica_periapical');
+            $table->text('observaciones_pertinentes');
+            $table->text('diagnostico');
+            $table->text('plan_tratamiento');
+            $table->string('registro_tension_arterial_pre_quirurgica');
+            $table->string('registro_tension_arterial_post_quirurgica');
+            $table->text('examenes_complementarios');
+            $table->text('consentimiento');
+            $table->text('evolucion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ficha_clinica_cirugia_paciente_regulars');
+    }
+};
