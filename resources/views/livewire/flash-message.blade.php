0fba261143805a8215f081d82b3cbf5ad6005f7a
Brian Caldera <brian_alex07@outlook.com>
2024-02-02 09:01:43 -0400

    Created flash message component.

---
+++ resources/views/livewire/flash-message.blade.php
@@ -0,0 +1,14 @@
+<div class="fixed bottom-0 right-0 p-4 w-full lg:w-1/4">
+    @if(session('message'))
+    <div class="bg-sky-200 rounded-lg p-4"
+        x-data="{ shown: false, timeout: null }"
+         x-init="() => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 5000); }"
+         x-show.transition.out.opacity.duration.1500ms="shown"
+         x-transition:leave.opacity.duration.1500ms
+         style="display: none;" >
+        <p>
+            {{ session('message') }}
+        </p>
+    </div>
+    @endif
+</div>
